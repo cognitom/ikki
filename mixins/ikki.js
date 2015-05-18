4cfbeeb63990fcaa05c0dbd9f75aa3a5a80ded2d
Tsutomu Kawamura <cognitom@gmail.com>
2015-05-18 21:48:40 +0900

    Refactoring a lot

--- mixins/ikki.js
+++ mixins/ikki.js
@@ -2,86 +2,100 @@
  * ikki mixin
  */
 
+// Replacing url query/param automatically
+// - route: #member/:id
+// - actual: #member/Tom
+// then, replace like below
+// <my-tag message="$name" /> -> <my-tag message="Tom" />
+function replaceRouteVar(str, route) {
+  if (!route) return str
+  Object.keys(route.query).map(function(key){
+    str = str.replace(new RegExp('^\\\?' + key + '$'), route.query[key])
+  })
+  Object.keys(route.param).map(function(key){
+    str = str.replace(new RegExp('^\\\$' + key + '$'), route.param[key])
+  })
+  return str
+}
+
 riot.mixin('ikki', {
+  $$version: 0,
+
   // initialize mixin
   init: function() {
     var self = this
-    var routeInfo = null
-    var opts = {}, opts_replaced = {}
+    var rt = self.$$findAncestor('route')
 
-    // Replacing url params automatically
-    // - route: #member/:id
-    // - actual: #member/Tom
-    // then, replace like below
-    // <my-tag message="$name" /> -> <my-tag message="Tom" />
-    function replaceParams(str){
-      if (!routeInfo) return str
-      var query = routeInfo.query
-      var param = routeInfo.param
-      Object.keys(query).map(function(key){
-        str = str.replace(new RegExp('^\\\?' + key + '$'), query[key])
+    if (rt){
+      // with routing
+      rt.on('urlchanged', function(route){
+        self.$$loadOpts(self.opts.opts, route)
+        self.one('update', function(){
+          Object.keys(self.opts).map(function(key) {
+            if ('string' == typeof self.opts[key])
+              self.opts[key] = replaceRouteVar(self.opts[key], route)
+          })
+        })
       })
-      Object.keys(param).map(function(key){
-        str = str.replace(new RegExp('^\\\$' + key + '$'), param[key])
-      })
-      return str
+    } else {
+      //without routing
+      self.$$loadOpts(self.opts.opts)
     }
+  },
 
-    function extend(obj, from) {
-      Object.keys(from).map(function(key) {
-        obj[key] = from[key]
-      })
-    }
+  $$findAncestor: function(tagName) {
+    tag = this
+    while (tag && tagName != tag.root.tagName.toLowerCase())
+      tag = tag.parent
+    return tag || null
+  },
 
-    self.on('update', function(){
-      if (self.opts.listeners && self.opts.listeners.length) {
-        self.opts.listeners.map(function(l) { self.on(l.key, l.callback) })
-        self.opts.listeners = []
-      }
-      routeInfo = self.getRouteInfo()
-      Object.keys(self.opts).map(function(key) {
-        if ('opts' != key) {
-          if (!opts[key] || opts_replaced[key] != self.opts[key])
-            opts[key] = self.opts[key]
-          self.opts[key] = replaceParams(opts[key])
-          opts_replaced[key] = self.opts[key]
-        }
-      })
-    })
-    var target = self.opts.opts
-    if ('object' == typeof target && !target.then) {
+  $$loadOpts: function(opts, route) {
+    if (!opts) return
+
+    var self = this
+    var version = ++self.$$version
+
+    if ('object' == typeof opts && !opts.then) {
+
       // 1: object
-      extend(self.opts, target)
-    } else if ('object' == typeof target) {
+      self.$$extendOpts(opts)
+
+    } else if ('object' == typeof opts) {
+
       // 2: promise
-      target.then(function(o) {
-        extend(self.opts, o)
-        self.update()
-      })
-    } else if (target && 'GeneratorFunction' == target.constructor.name) {
+      opts.then(function(o) { self.$$extendOpts(o) && self.update() })
+
+    } else if (opts && 'GeneratorFunction' == opts.constructor.name) {
+
       // 3: generator
       function process(gen) {
         var prom = gen.next().value
-        if (!prom) return
-        prom.then(function(o) {
-          extend(self.opts, o)
-          self.update()
-          setTimeout(function(){
-            process(gen)
-          }, 0)
-        })
+        function setAndGo(o) {
+          if (version < self.$$version) return
+          self.$$extendOpts(o) && self.update()
+          setTimeout(function(){ process(gen) }, 0)
+        }
+        if (!prom) return // end of chain
+        if (!prom.then) setAndGo(prom)
+        else prom.then(setAndGo)
       }
-      process(target())
+      process(route
+        ? opts(route.path, route.query, route.param)
+        : opts('', {}, {})
+      )
+
     }
   },
-  getRouteInfo: function() {
-    tag = this
-    while (tag && 'route' != tag.root.tagName.toLowerCase()) tag = tag.parent
-    return !tag ? null
-                : {
-                    path: tag.parent.path || '',
-                    query: tag.parent.query || {},
-                    param: tag.param || {}
-                  }
+
+  $$extendOpts: function(opts) {
+    var self = this
+    if (opts.listeners){
+      opts.listeners.map(function(l) { self.on(l.key, l.callback) })
+      delete opts.listeners
+    }
+    var keys = Object.keys(opts)
+    keys.map(function(key) { self.opts[key] = opts[key] })
+    return keys.length > 0
   }
 })
